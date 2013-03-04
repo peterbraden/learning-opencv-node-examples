e72b9850da63e884798fbc1c3634521ebb148716
Peter Braden <peterbraden@peterbraden.co.uk>
2013-03-04 11:32:41 -0800

    args 2 as running "node ch2_ex2_1.js"

--- ch2_ex2_1.js
+++ ch2_ex2_1.js
@@ -1,6 +1,6 @@
 var cv = require('opencv')
 
-cv.readImage(process.argv[1], function(err, im){
+cv.readImage(process.argv[2], function(err, im){
   var win = new cv.NamedWindow("Example1")
 
   win.show(im);
